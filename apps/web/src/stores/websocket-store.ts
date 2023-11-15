d08bd2061ff6f03c5808314fb215f18ad141b5b4
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-11-15 13:40:42 -0800

    Add try/catch to websocket connect

--- apps/web/src/stores/websocket-store.ts
+++ apps/web/src/stores/websocket-store.ts
@@ -47,95 +47,81 @@ async function connect(set: WebsocketStoreSet, get: () => WebsocketStore) {
     throw new Error('NEXT_PUBLIC_BACKEND_URL environment variable is not set.');
   }
 
-  // Clear existing socket (if any)
-  const { socket } = get();
-  if (socket) {
-    socket.close();
-    set({ socket: null });
-  }
-
-  // Fetch websocket auth token
-  let tokenRequest = await fetch('/api/websocket/', {
-    method: 'POST',
-  });
-
-  if (tokenRequest.status !== 200) {
-    console.error('Unable to fetch websocket token.');
-    return;
-  }
-
-  let response = await tokenRequest.json();
-
-  // Create connectionId if it doesn't exist
-  let connectionId = get().connectionId || Math.random().toString(36).substring(2, 15);
-  set({ connectionId });
-
-  // Intialize websocket connection
-  let ws = new WebSocket(`${process.env.NEXT_PUBLIC_BACKEND_URL}?token=${response.token}&connectionId=${connectionId}`);
-
-  set({ socket: ws, socketState: 'connecting' });
-
-  ws.addEventListener('open', () => {
-    // Send heartbeat every 30 seconds
-    const heartbeat = setInterval(() => {
-      const { isAlive, sendToServer } = get();
-
-      if (!isAlive) {
-        console.error('Client heartbeat request failed to recieve response. Closing connection.');
-        ws.close();
-        return;
-      }
-
-      set({ isAlive: false });
-
-      sendToServer({
-        type: StarlightWebSocketRequestType.heartbeatClientRequest,
-        data: {
-          timestamp: Date.now(),
-        },
-      });
-    }, 30000);
-
-    set({ socketState: 'open', exponentialBackoff: 1000, isAlive: true, heartbeat });
-  });
-
-  ws.addEventListener('message', (event) => {
-    try {
-      const response = validateResponse(event.data);
-      if (!response) return;
-
-      const handler = handlers[response.type as keyof typeof handlers];
-      if (!handler) {
-        console.error(`No handler found for response type: ${response.type}`);
-        return;
-      }
-
-      handler(response);
-    } catch (error) {
-      console.error('Error in handling WebSocket message:', error);
-      Sentry.captureException(error);
+  try {
+    // Clear existing socket (if any)
+    const { socket } = get();
+    if (socket) {
+      socket.close();
+      set({ socket: null });
     }
-  });
 
-  ws.addEventListener('error', (event) => {
-    console.error('WebSocket error:', event);
-    Sentry.captureException(event, {
-      contexts: {
-        websocket: {
-          socket: ws,
-          event: event,
-          eventJSON: JSON.stringify(event),
-        },
-      },
+    // Fetch websocket auth token
+    let tokenRequest = await fetch('/api/websocket/', {
+      method: 'POST',
     });
-  });
 
-  ws.addEventListener('close', (event) => {
-    set({ socket: null, socketState: 'closed', isAlive: false, heartbeat: null });
+    if (tokenRequest.status !== 200) {
+      console.error('Unable to fetch websocket token.');
+      return;
+    }
 
-    console.log(`WebSocket connection closed. Code: ${event.code} Reason: ${event.reason}`);
+    let response = await tokenRequest.json();
 
-    if (!event.wasClean) {
+    // Create connectionId if it doesn't exist
+    let connectionId = get().connectionId || Math.random().toString(36).substring(2, 15);
+    set({ connectionId });
+
+    // Intialize websocket connection
+    let ws = new WebSocket(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}?token=${response.token}&connectionId=${connectionId}`,
+    );
+
+    set({ socket: ws, socketState: 'connecting' });
+
+    ws.addEventListener('open', () => {
+      // Send heartbeat every 30 seconds
+      const heartbeat = setInterval(() => {
+        const { isAlive, sendToServer } = get();
+
+        if (!isAlive) {
+          console.error('Client heartbeat request failed to recieve response. Closing connection.');
+          ws.close();
+          return;
+        }
+
+        set({ isAlive: false });
+
+        sendToServer({
+          type: StarlightWebSocketRequestType.heartbeatClientRequest,
+          data: {
+            timestamp: Date.now(),
+          },
+        });
+      }, 30000);
+
+      set({ socketState: 'open', exponentialBackoff: 1000, isAlive: true, heartbeat });
+    });
+
+    ws.addEventListener('message', (event) => {
+      try {
+        const response = validateResponse(event.data);
+        if (!response) return;
+
+        const handler = handlers[response.type as keyof typeof handlers];
+        if (!handler) {
+          console.error(`No handler found for response type: ${response.type}`);
+          return;
+        }
+
+        handler(response);
+      } catch (error) {
+        console.error('Error in handling WebSocket message:', error);
+        Sentry.captureException(error);
+      }
+    });
+
+    ws.addEventListener('error', (event) => {
+      console.error('WebSocket error:', event);
       Sentry.captureException(event, {
         contexts: {
           websocket: {
@@ -145,13 +131,39 @@ async function connect(set: WebsocketStoreSet, get: () => WebsocketStore) {
           },
         },
       });
-    }
+    });
+
+    ws.addEventListener('close', (event) => {
+      set({ socket: null, socketState: 'closed', isAlive: false, heartbeat: null });
+
+      console.log(`WebSocket connection closed. Code: ${event.code} Reason: ${event.reason}`);
+
+      if (!event.wasClean) {
+        Sentry.captureException(event, {
+          contexts: {
+            websocket: {
+              socket: ws,
+              event: event,
+              eventJSON: JSON.stringify(event),
+            },
+          },
+        });
+      }
+
+      setTimeout(() => {
+        set({ exponentialBackoff: get().exponentialBackoff * 2 });
+        get().connect();
+      }, get().exponentialBackoff);
+    });
+  } catch (error) {
+    console.error('Error in connecting to WebSocket:', error);
+    Sentry.captureException(error);
 
     setTimeout(() => {
       set({ exponentialBackoff: get().exponentialBackoff * 2 });
       get().connect();
     }, get().exponentialBackoff);
-  });
+  }
 }
 
 function sendToServer(get: () => WebsocketStore, request: StarlightWebSocketRequest) {
