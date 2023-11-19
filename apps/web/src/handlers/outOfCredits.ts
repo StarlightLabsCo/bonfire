c075acbf124703da52ba690599ece3dae4f10eb0
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-11-18 18:31:34 -0800

    Fix out of credits message handling (clears submittedMessage)

--- apps/web/src/handlers/outOfCredits.ts
+++ apps/web/src/handlers/outOfCredits.ts
@@ -1,8 +1,10 @@
 import { useDialogStore } from '@/stores/dialog-store';
+import { useMessagesStore } from '@/stores/messages-store';
 import { StarlightWebSocketResponse, StarlightWebSocketResponseType } from 'websocket';
 
 export function handleOutOfCredits(response: StarlightWebSocketResponse) {
   if (response.type === StarlightWebSocketResponseType.outOfCredits) {
     useDialogStore.setState({ isCreditsDialogOpen: true });
+    useMessagesStore.getState().setSubmittedMessage(null);
   }
 }
