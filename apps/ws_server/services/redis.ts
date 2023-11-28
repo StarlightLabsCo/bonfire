065e60fe05e9570d32b00da9aaa441f7da36d953
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-11-27 19:04:59 -0800

    Add Redis private url.

--- apps/ws_server/services/redis.ts
+++ apps/ws_server/services/redis.ts
@@ -1,11 +1,4 @@
 // Documentation: https://www.npmjs.com/package/ioredis
 import Redis from 'ioredis';
 
-const redisURL = new URL(process.env.REDIS_PRIVATE_URL as string);
-
-export const redis = new Redis({
-  host: redisURL.hostname,
-  port: Number(redisURL.port),
-  username: redisURL.username,
-  password: redisURL.password,
-});
+export const redis = new Redis(process.env.REDIS_PRIVATE_URL as string);
