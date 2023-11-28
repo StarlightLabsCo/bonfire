df0ec8f82669f98d3e91a2ccbe5b8387f6e548ca
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-11-27 18:56:49 -0800

    Swap to private networking redis.

--- apps/ws_server/services/redis.ts
+++ apps/ws_server/services/redis.ts
@@ -1,9 +1,4 @@
 // Documentation: https://www.npmjs.com/package/ioredis
 import Redis from 'ioredis';
 
-export const redis = new Redis({
-  port: process.env.REDISPORT ? parseInt(process.env.REDISPORT) : 6379,
-  host: process.env.REDISHOST || 'localhost',
-  username: process.env.REDISUSER,
-  password: process.env.REDISPASSWORD,
-});
+export const redis = new Redis(process.env.REDIS_PRIVATE_URL as string);
