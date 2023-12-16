506f52714b4350046a891cfbcc7661730171820c
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-12-15 17:22:15 -0800

    Fixed sql command.

--- apps/web/src/app/(app)/instances/[id]/opengraph-image.tsx
+++ apps/web/src/app/(app)/instances/[id]/opengraph-image.tsx
@@ -11,37 +11,39 @@ export const size = {
 export const contentType = 'image/png';
 
 export default async function Image({ params }: { params: { id: string } }) {
+  console.log(`Generating image for instance ${params.id}`);
   const client = createClient({
     connectionString: process.env.DATABASE_URL,
   });
 
   await client.connect();
 
+  console.log(`Connected to database.`);
   const { rows } = await client.query(
     `
     SELECT m.content
     FROM "Message" m
-    INNER JOIN "Instance" i ON m.instanceId = i.id
-    WHERE m.instanceId = $1
+    INNER JOIN "Instance" i ON m."instanceId" = i.id
+    WHERE m."instanceId" = $1
     AND m.role = 'function'
     AND m.name = 'generate_image'
     AND i.public = true
     AND m.content LIKE 'https://%'
-    ORDER BY m.createdAt ASC
+    ORDER BY m."createdAt" ASC
     LIMIT 1
   `,
     [params.id],
   );
 
-  const image = rows[0];
+  const result = rows[0];
 
-  if (!image) {
+  if (!result) {
     throw new Error('Image not found');
   }
 
   await client.end();
 
-  return new ImageResponse(<img src={image.content} alt={alt} width={size.width} height={size.height} />, {
+  return new ImageResponse(<img src={result.content} alt={alt} width={size.width} height={size.height} />, {
     ...size,
   });
 }
