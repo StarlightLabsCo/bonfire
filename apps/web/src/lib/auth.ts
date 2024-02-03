1ebc7103730f515c7709c66b67c163ae6e4b054b
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-02-02 21:24:24 -0800

    Add PCKE verifier cookies.

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -96,4 +96,24 @@ export const authOptions = {
       }
     },
   },
+  cookies: {
+    csrfToken: {
+      name: 'next-auth.csrf-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+    pkceCodeVerifier: {
+      name: 'next-auth.pkce.code_verifier',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+  },
 };
