7e07730c94118a26d983050e69fdfbd151a406ea
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-02-08 14:52:05 -0800

    Change pause icon to heroicons.

--- apps/web/src/components/icons.tsx
+++ apps/web/src/components/icons.tsx
@@ -485,13 +485,15 @@ export const Icons = {
     </svg>
   ),
   pause: (props: IconProps) => (
-    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" {...props}>
       <path
-        d="M6.04995 2.74998C6.04995 2.44623 5.80371 2.19998 5.49995 2.19998C5.19619 2.19998 4.94995 2.44623 4.94995 2.74998V12.25C4.94995 12.5537 5.19619 12.8 5.49995 12.8C5.80371 12.8 6.04995 12.5537 6.04995 12.25V2.74998ZM10.05 2.74998C10.05 2.44623 9.80371 2.19998 9.49995 2.19998C9.19619 2.19998 8.94995 2.44623 8.94995 2.74998V12.25C8.94995 12.5537 9.19619 12.8 9.49995 12.8C9.80371 12.8 10.05 12.5537 10.05 12.25V2.74998Z"
         fill="currentColor"
         fillRule="evenodd"
         clipRule="evenodd"
-      ></path>
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+      />
     </svg>
   ),
 };
