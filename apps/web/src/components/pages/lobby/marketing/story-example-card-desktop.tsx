81b920c64de3832fe157206f3e51a60853ed1f82
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-03-05 16:57:18 -0800

    Adjust border radius on card.

--- apps/web/src/components/pages/lobby/marketing/story-example-card-desktop.tsx
+++ apps/web/src/components/pages/lobby/marketing/story-example-card-desktop.tsx
@@ -21,7 +21,7 @@ export function StoryExampleCardDesktop({ id, isActive, title, description, imag
         className={cn('absolute z-10 w-full h-full bg-black/90 transition-opacity duration-500 ease-in-out rounded-xl')}
         style={{ opacity: isActive ? 0 : 0.8 }}
       />
-      <Image className="object-cover h-full rounded-xl aspect-9/16" src={image} alt={title} width={1792} height={1024} />
+      <Image className="object-cover h-full rounded-[11px] aspect-9/16" src={image} alt={title} width={1792} height={1024} />
       <div className="absolute bottom-0 w-full h-1/4 bg-black/90 rounded-b-xl" />
       <div className="absolute w-full bottom-1/4 h-1/4 bg-gradient-to-t from-black/90 to-transparent" />
       <div className="absolute bottom-0 z-10 flex flex-col justify-between w-full h-48 p-3 mb-3 gap-y-1">
